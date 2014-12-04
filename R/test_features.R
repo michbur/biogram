599581bc2d784e2c24111e245dcaef7dda122b97
Piotr Sobczyk <sobbombo@gmail.com>
2014-12-04 12:44:24 +0100

    removing functions computing multinomial pdf - using basic dmultinom function from now on

--- R/test_features.R
+++ R/test_features.R
@@ -1,56 +1,16 @@
-#'Calculate log of partial binomial probabilities 
-#'
-#'@param p probability of success
-#'@param m number of trials
-#'@param n number of successes
-#'
-#'@return temp numeric, log(m!/[(m-n)! n!] p^n
-#'@keywords internal
-calc_partial_binomial_log_probability <- function(p, m, n) { 
-  temp = lgamma(m + 1.0);
-  temp = temp - lgamma(n + 1.0) - lgamma(m - n + 1.0);
-  temp = temp + n*log(p);
-  temp
-}
-
-#' Calculate the probability of a certain contingency table
-#' 
-#' Contingency table for two Bernoulli random variables. Feature and
-#' target. 
-#' 
-#' @param p numeric, a probability of feature equaling 1
-#' @param q numeric, a probability of feature equaling 1
-#' @param n11 number of elements for which both target and feature equal 1
-#' @param n01 number of elements for which both target and feature equal 1,0
-#' @param n10 number of elements for which both target and feature equal 0,1
-#' @param n00 number of elements for which both target and feature equal 0
-#' 
-#' @keywords internal
-#' @return log_probability probability of getting given contingency table
-#'         under p and q
-calc_cont_table_log_probability <- function(p, q, n11, n01, n10, n00) {
-  n <- n11 + n01 + n10 + n00
-  log_probability <- calc_partial_binomial_log_probability(p*q, n, n11)
-  log_probability <- log_probability + 
-    calc_partial_binomial_log_probability((1-p)*q, n-n11, n01)
-  log_probability <- log_probability + 
-    calc_partial_binomial_log_probability(p*(1-q), n-n11-n01, n10)
-  log_probability <- log_probability + 
-    calc_partial_binomial_log_probability((1-p)*(1-q), n-n11-n01-n10, n00)
-  return(log_probability)
-}
-
-#' Create feature with given contingency matrix
+#' Create feature according to given contingency matrix
 #'
 #' @param n11 number of elements for which both target and feature equal 1
-#' @param n01 number of elements for which both target and feature equal 1,0
-#' @param n10 number of elements for which both target and feature equal 0,1
+#' @param n01 number of elements for which target and feature equal 1,0 
+#' respectively
+#' @param n10 number of elements for which target and feature equal 0,1 
+#' respectively
 #' @param n00 number of elements for which both target and feature equal 0
 #' @export
 #' @return a matrix of 2 columns and n11+n10+n01+n00 rows. First column is
 #' target, second is feature
 #' @examples
-#' temp <- create_feature_target(10, 375, 15, 600)
+#' target_feature <- create_feature_target(10, 375, 15, 600)
 create_feature_target <- function(n11, n01, n10, n00){
   tar <- rep(1, n11)
   feat <- rep(1, n11)
@@ -67,14 +27,17 @@ create_feature_target <- function(n11, n01, n10, n00){
 #' 
 #' @param target {0,1}-valued target vector
 #' @param feature {0,1}-valued feature vector
-#' @param graphical.output default value is FALSE
+#' @param graphical.output default value is FALSE, if TRUE
+#'        probability density function is plotted
 #' @export
 #' @return a matrix of 3 rows. First row are possible IG values, 
 #'  second in probability density function, third is cimulative 
 #'  distribution function
-#'  @examples
-#' temp <- create_feature_target(10, 375, 15, 600) 
-#' ig_distribution(temp[,1], temp[,2], graphical.output = TRUE)
+#' @seealso \code{\link{calc_ig}}
+#' @examples
+#' target_feature <- create_feature_target(10, 375, 15, 600) 
+#' ig_distribution(target = target_feature[,1], feature = target_feature[,2], 
+#' graphical.output = TRUE)
 ig_distribution <- function(target, feature, graphical.output = FALSE) {
   n <- length(target)
   if (length(feature) != n) {
@@ -95,9 +58,11 @@ ig_distribution <- function(target, feature, graphical.output = FALSE) {
   q <- non_zero_feat/n
   for (i in 0:min(non_zero_target,non_zero_feat)){
     prob_log[i+1] <- 
-      calc_cont_table_log_probability(p, q, i, non_zero_feat-i, 
-                                      non_zero_target-i, 
-                                      n-non_zero_target-non_zero_feat+i)
+      dmultinom(x = c(i, non_zero_feat-i, non_zero_target-i, 
+                  n-non_zero_target-non_zero_feat+i),
+                size = n,
+                prob = c(p*q, (1-p)*q, p*(1-q), (1-p)*(1-q)),
+                log = TRUE)
     dane <- create_feature_target(i, non_zero_feat-i, non_zero_target-i,
                                   n-non_zero_target-non_zero_feat+i)
     ig[i+1] <- calc_ig(dane[,1], dane[,2, drop=F])
@@ -121,6 +86,8 @@ ig_distribution <- function(target, feature, graphical.output = FALSE) {
            horiz = TRUE, fill=c("red", "green"),  bty = "n", cex = 1)
   }
   
+  # We get the same IG values for different contingency tables
+  # therefore we need to combine them for distribution
   ig_dist_temp <- ig_dist_temp[order(ig)]
   ig_val_temp <- ig[order(ig)]
   j <- 1
@@ -155,17 +122,17 @@ ig_distribution <- function(target, feature, graphical.output = FALSE) {
 #' \itemize{
 #' \item{"\code{ig}" - information gain}
 #' }
-#' @return a numeric vector of lenth equal to the number of features containing computed
-#' information gain values.
+#' @return a vector of objects of htest class that relate to each feature tested
 #' @note Both \code{target} and \code{features} must be binary, i.e. contain only 0 
 #' and 1 values.
 #' @seealso \code{\link{calc_ig}}, \code{\link{ig_distribution}}
 #' @export
 #' @examples
-#' temp <- create_feature_target(10, 390, 0, 600) 
-#' temp2 <- create_feature_target(9, 391, 1, 599)
-#' temp3 <- create_feature_target(8, 392, 0, 600)
-#' test_features_fast(temp[,1], cbind(temp[,2], temp2[,2], temp3[,2]))
+#' tar_feat1 <- create_feature_target(10, 390, 0, 600) 
+#' tar_feat2 <- create_feature_target(9, 391, 1, 599)
+#' tar_feat3 <- create_feature_target(8, 392, 0, 600)
+#' test_features_fast(tar_feat1[,1], cbind(tar_feat1[,2], tar_feat2[,2], 
+#' tar_feat3[,2]))
 test_features_fast <- function(target, features, criterion = "ig") {
   feature_size <- unique(apply(features, 2, function(feature) {sum(feature)}))
   dists <- lapply(feature_size, function(i){
