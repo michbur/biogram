912824f30b4a576fd97aa388cb4f1083e7a05162
Piotr Sobczyk <sobbombo@gmail.com>
2014-12-11 11:21:13 +0100

    removing test for deleted function. Adding test for consistency between permuation test and exact test

---
+++ tests/testthat/test_quit_consistency.R
@@ -0,0 +1,26 @@
+context("Exact test and distribution under H0")
+
+test_that("Exact test and permutation test are consistent",{
+  # Note that this test might fail 1 in 100 times as we build
+  # confidence interavals
+  tar_feat1 <- create_feature_target(10, 390, 0, 600) 
+  tar_feat2 <- create_feature_target(9, 391, 1, 599)
+  tar_feat3 <- create_feature_target(8, 392, 2, 598)
+  fast.results <- test_features_fast(tar_feat1[,1], 
+                     cbind(tar_feat1[,2], tar_feat2[,2], tar_feat3[,2]))
+  
+  m <- 10000
+  perm.results <- test_features(target_feature[, 1, drop=FALSE], 
+                cbind(tar_feat1[,2], tar_feat2[,2], tar_feat3[,2]),
+                m)
+  alfa <- 0.01
+  conf.intervals <- sapply(perm.results, 
+                           function(x) {
+                             x <- (x*m+0.5*qnorm(alfa)^2)/(m+qnorm(alfa)^2)
+                             x+qnorm(c(alfa/2,1-alfa/2))*sqrt(x*(1-x)/(m+qnorm(alfa)^2))})
+  
+  for (i in 1:3) {
+    expect_true(as.numeric(fast.results[,i][["p.value"]]) < conf.intervals[2,i])
+    expect_true(as.numeric(fast.results[,i][["p.value"]]) > conf.intervals[1,i])
+  } 
+})
