a039c566af49dab8ea3955ccef5a19e3ae6da721
michbur <michalburdukiewicz@gmail.com>
2015-04-17 13:26:28 +0200

    added gaps to construct_ngrams

--- R/construct_ngrams.R
+++ R/construct_ngrams.R
@@ -5,12 +5,15 @@
 #' @param target \code{integer} vector with target information (e.g. class labels).
 #' @inheritParams count_ngrams
 #' @param n_max size of constructed n-grams.
+#' @param conf_level confidence level for discriminating between significant and 
+#' non-significant features. 
+#' @param gap \code{logical}, if \code{TRUE} gap are used IMPROVE ME.
 #' @return a vector of n-grams.
 #' @details \code{construct_ngrams} extracts unigrams from the sequences and filters 
-#' significant features. From filtered unigrams, the function builds all possible 
-#' bigrams using \code{\link{add_1grams}} and so on, till the required size of 
-#' the n-gram is achieved. On each step, obtained n-grams are filtered using 
-#' \code{\link[QuiPT]{test_features}}.
+#' significant features (with p-value below \code{conf_level}). From filtered unigrams, 
+#' the function builds all possible bigrams using \code{\link{add_1grams}} and so on, 
+#' till the required size of the n-gram is achieved. On each step, obtained n-grams 
+#' are filtered using \code{\link[QuiPT]{test_features}}.
 #' @export
 #' @examples
 #' \donttest{
@@ -23,12 +26,12 @@
 #' bigrams <- construct_ngrams(human_cleave[, "tar"], deg_seqs, 1L:5, 2)
 #' }
 
-construct_ngrams <- function(target, seq, u, n_max) {
+construct_ngrams <- function(target, seq, u, n_max, conf_level = 0.95, gap = TRUE) {
   #build unigrams
   unigrams <- count_ngrams(seq, 1, u, pos = TRUE)
   #filter unigrams
   test_unigrams <- test_features(target, unigrams)
-  signif_ngrams <- cut(test_unigrams, breaks = c(0, 0.05, 1))[[1]]
+  signif_ngrams <- cut(test_unigrams, breaks = c(0, 1 - conf_level, 1))[[1]]
   
   res <- list()
   
@@ -36,7 +39,12 @@ construct_ngrams <- function(target, seq, u, n_max) {
   
   for(i in 2L:n_max) {
     if(length(signif_ngrams) != 0) {
-      signif_ngrams <- build_and_test(signif_ngrams, seq, i, u, target)
+      signif_ngrams <- if(i > 2 && gap) {
+        build_and_test_gaps(signif_ngrams, seq, i, u, target)
+      } else {
+        build_and_test(signif_ngrams, seq, i, u, target)
+      }
+          
       res[[i]] <- signif_ngrams
     } else {
       NULL
@@ -49,5 +57,21 @@ build_and_test <- function(signif_ngrams, seq, n, u, target) {
   nplusgrams <- unique(add_1grams(signif_ngrams))
   new_counts <- count_specified(seq, nplusgrams)
   new_test <- test_features(target, new_counts)
+  cut(new_test, breaks = c(0, 0.05, 1))[[1]]
+}
+
+build_and_test_gaps <- function(signif_ngrams, seq, n, u, target) {
+  nplusgrams <- unique(add_1grams(signif_ngrams))
+  gap_nplusgrams <- gap_ngrams(nplusgrams)
+  new_counts <- count_specified(seq, gap_nplusgrams)
+  
+  #artificial counts created by sum of counts of all gapped n-grams belonging to n-gram
+  art_counts <- do.call(cbind, lapply(1L:length(nplusgrams), function(id_ngram) {
+    as.numeric(row_sums(new_counts[, n*(id_ngram - 1) + 1:n]) > 0)
+  }))
+  
+  colnames(art_counts) <- nplusgrams
+  
+  new_test <- test_features(target, art_counts)
   cut(new_test, breaks = c(0, 0.05, 1))[[1]]
 }
