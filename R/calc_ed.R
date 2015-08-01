e6ddf0187d4152e9e2510e3fdd6f17f3f030ca65
michbur <michalburdukiewicz@gmail.com>
2015-08-01 02:40:49 +0200

    added calc_ed to the main document

--- R/calc_ed.R
+++ R/calc_ed.R
@@ -1,6 +1,6 @@
 #' Calculate encoding distance
 #' 
-#' Compares two encodings and computes encoding distance between them.
+#' Computes encoding distance between two encodings.
 #' @param a an encoding (see Note)
 #' @param b an encoding to which \code{a} should be compared. Must be have less groups than 
 #' \code{a} or have equal length.
@@ -11,6 +11,7 @@
 #' @return an encoding distance.
 #' @export
 #' @examples
+#' #calculate encoding distance between two encodings of amino acids
 #' aa1 = list(`1` = c("g", "a", "p", "v", "m", "l", "i"), 
 #'            `2` = c("k", "h"), 
 #'            `3` = c("d", "e"), 
@@ -19,8 +20,11 @@
 #' aa2 = list(`1` = c("g", "a", "p", "v", "m", "l", "q"), 
 #'            `2` = c("k", "h", "d", "e", "i"), 
 #'            `4` = c("f", "r", "w", "y", "s", "t", "c", "n"))
-#' calc_ed(aa1, aa2)     
+#' calc_ed(aa1, aa2) 
 #'     
+#' #the encoding distance between two identical encodings is 0
+#' calc_ed(aa1, aa1) 
+#'  
 
 calc_ed <- function(a, b) {
   #compare temporary a to temporary b
