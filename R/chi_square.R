3d1e877f725430d029ec7a72621929d8f86a2df0
michbur <michalburdukiewicz@gmail.com>
2016-02-04 12:01:56 +0100

    docs clarified

--- R/chi_square.R
+++ R/chi_square.R
@@ -15,12 +15,10 @@
 #' @examples tar <- sample(0L:1, 100, replace = TRUE)
 #' feat <- sample(0L:1, 100, replace = TRUE)
 #' prop <- c(100 - sum(tar), sum(tar))/100
-#' entr <- - sum(prop*log(prop))
 #' library(bit) #used to code vector as bit
-#' calc_chisq(feat, as.bit(tar), 100, sum(tar), entr)
-calc_chisq <- function(feature, target_b, len_target, pos_target, ES) {
+#' calc_chisq(feat, as.bit(tar), 100, sum(tar))
+calc_chisq <- function(feature, target_b, len_target, pos_target) {
   crosstable <- fast_crosstable(target_b, len_target, pos_target, feature)
   counts_feature <- c(crosstable[2] + crosstable[4], crosstable[1] + crosstable[3])
   
-  browser()
 }
