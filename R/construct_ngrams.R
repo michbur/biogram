74ef7d93d8921e53ca9cae17d0947860cd3ce56f
michbur <michalburdukiewicz@gmail.com>
2016-03-11 14:36:19 +0100

    construct n-grams example now faster

--- R/construct_ngrams.R
+++ R/construct_ngrams.R
@@ -24,15 +24,16 @@
 #' @seealso
 #' Feature filtering method: \code{\link{test_features}}.
 #' @examples
-#' \donttest{
-#' deg_seqs <- degenerate(human_cleave[, 1L:9],
+#' # to make the example faster, we run construct_ngrams() on the 
+#' # subset of data
+#' deg_seqs <- degenerate(human_cleave[c(1L:100, 801L:900), 1L:9],
 #' list(`1` = c(1, 6, 8, 10, 11, 18),
 #'      `2` = c(2, 13, 14, 16, 17),
 #'      `3` = c(5, 19, 20),
 #'      `4` = c(7, 9, 12, 15),
 #'      '5' = c(3, 4)))
-#' bigrams <- construct_ngrams(human_cleave[, "tar"], deg_seqs, 1L:5, 2)
-#' }
+#' bigrams <- construct_ngrams(human_cleave[c(1L:100, 801L:900), "tar"], deg_seqs, 1L:5, 2)
+
 construct_ngrams <- function(target, seq, u, n_max, conf_level = 0.95, gap = TRUE) {
   # build unigrams
   unigrams <- count_ngrams(seq, 1, u, pos = TRUE)
