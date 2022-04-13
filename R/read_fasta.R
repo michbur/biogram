71b4774f1380a34407feef308349a209af3efb89
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2022-04-13 16:58:00 +0200

    updated sequence reading

--- R/read_fasta.R
+++ R/read_fasta.R
@@ -15,8 +15,9 @@
 
 read_fasta <- function(file) {
   all_lines <- readLines(file)
-  prot_id <- cumsum(grepl("^>", all_lines))
-  all_prots <- split(all_lines, prot_id)
+  nonempty_lines <- all_lines[which(all_lines != "")]
+  prot_id <- cumsum(grepl("^>", nonempty_lines))
+  all_prots <- split(nonempty_lines, prot_id)
 
   seq_list <- lapply(all_prots, function(ith_seq)
     unlist(strsplit(ith_seq[-1], split = "")))
@@ -43,4 +44,56 @@ write_fasta <- function(seq, file, nchar = 80) {
     c(paste0(">", names(seq[ith_id])), 
       lapply(split(seq[[ith_id]], floor(seq_along(seq[[ith_id]])/nchar)), paste0, collapse = ""))))
   writeLines(text = char_vec, con = file)
+}
+
+
+#' Read sequences from .txt file
+#'
+#' Read sequence data saved in text file.
+#'
+#' @param connection a \code{\link{connection}} to the text (.txt) file.
+#' @return a list of sequences. 
+#' @details The input file should contain one or more amino acid sequences separated by 
+#' empty line(s).
+#' @export
+#' @keywords manip
+#' @examples 
+#' sequences <- read_txt(system.file("PlastoGram/sequences.txt", package = "PlastoGram"))
+
+read_txt <- function(connection) {
+  
+  content <- readLines(connection)
+  
+  #test for empty content
+  if(content[1] != "" || length(content) > 1) {
+    if (sum(grepl(">", content, fixed = TRUE)) == 0) {
+      if (content[1] != "")
+        content <- c("", content)
+      
+      #number of empty lines
+      nel <- 0
+      #content without too many empty lines
+      content2 <- c()
+      for (i in 1L:length(content)) {
+        if(content[i] == "") {
+          nel <- nel + 1
+        } else {
+          nel <- 0
+        }
+        if (nel <= 1)
+          content2 <- c(content2, content[i])
+      }
+      content <- content2
+      content_end <- length(content)
+      while(content[content_end] == "i")
+        content_end <- content_end - 1
+      prot_names <- sapply(1L:sum(content == ""), function(i)
+        paste0(">sequence", i))
+      content[content == ""] <- prot_names
+    }
+    read_fasta(textConnection(content))
+  } else {
+    warning("No text detected.")
+    NULL
+  } 
 }
